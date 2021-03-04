3f9cf2137570a390e14d8d1fadf8d9ad1ca83598
AlexSzabo08 <alexszabo08bs@gmail.com>
2021-03-04 18:13:31 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+
+import { ethers } from 'ethers'
 
 export default function App() {
+
+  const [input, setinput] = useState('')
+  
+  const [txt, settxt] = useState('')
+
+  const [rebotContract, setContract] = useState()
+
+  //load ether
+  useEffect(() => {
+    async function load () {
+    let wallet = ethers.Wallet.fromMnemonic('sea peace bike clock whip caution fiber wrist force pair disease release')
+    const provider = new ethers.getDefaultProvider('ropsten')
+    wallet = wallet.connect(provider)
+
+    const ABI = [
+      'function nmb(uint8 _nr) public',
+      'function str(string memory _string) public view returns(string memory)'
+    ]
+    const contract = new ethers.Contract('0xc5F13aE40e06114eDe1d250D526838Cc7D2E85D4',
+                                          ABI,
+                                          provider)
+
+    setContract(contract)
+    }
+    load()
+  }, [])
+
+  const xd = async () => {
+    const data = await rebotContract.str(input)
+
+    settxt(data)
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setinput(text)}
+        value={input}
+      />
+      <Button title="PRESS" onPress={xd}/>
+      <Text>
+        {txt}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
